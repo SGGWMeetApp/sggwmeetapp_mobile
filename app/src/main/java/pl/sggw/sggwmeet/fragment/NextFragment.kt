8a38d8aeb05ab1314b60053ba401c74986778f55
Dominik Derewońko <dominikderewonko@gmail.com>
2022-11-17 18:52:44 +0100

    Basic UI test

--- app/src/main/java/pl/sggw/sggwmeet/fragment/NextFragment.kt
+++ app/src/main/java/pl/sggw/sggwmeet/fragment/NextFragment.kt
@@ -2,22 +2,25 @@ package pl.sggw.sggwmeet.fragment
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import pl.sggw.sggwmeet.MainActivity
 import pl.sggw.sggwmeet.R
 import pl.sggw.sggwmeet.databinding.FragmentNextBinding
-import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class NextFragment: Fragment() {
     private lateinit var binding: FragmentNextBinding
 
-    @Inject
-    lateinit var injectedString : String
+//    @Inject
+//    lateinit var injectedString : String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         this.binding = FragmentNextBinding.inflate(inflater, container, false)
@@ -26,8 +29,9 @@ class NextFragment: Fragment() {
             Log.i("SGGWMA " + this::class.simpleName, "moving to StartFragment...")
             this.findNavController().navigate(R.id.action_nextFragment_to_startFragment)
         }
+        (activity as AppCompatActivity?)!!.supportActionBar!!.subtitle = "Next"
 
-        this.binding.labelInjected.text = injectedString
+//        this.binding.labelInjected.text = injectedString
 
         return this.binding.root
     }
---
+++ app/src/main/java/pl/sggw/sggwmeet/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package pl.sggw.sggwmeet.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
