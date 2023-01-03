ecc81f5c00a9d5ac4a6710c5ff8a3a0a7d165d1d
Dominik Derewońko <dominikderewonko@gmail.com>
2023-01-03 17:31:23 +0100

    User info activity to show, well, detailed user info when picking users.

--- app/src/main/java/pl/sggw/sggwmeet/adapter/GroupUserAddAdapter.kt
+++ app/src/main/java/pl/sggw/sggwmeet/adapter/GroupUserAddAdapter.kt
@@ -1,6 +1,7 @@
 package pl.sggw.sggwmeet.adapter
 
 import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.activity.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import pl.sggw.sggwmeet.R
+import pl.sggw.sggwmeet.activity.UserShowActivity
 import pl.sggw.sggwmeet.activity.group.GroupAddUserListActivity
 import pl.sggw.sggwmeet.exception.ClientException
 import pl.sggw.sggwmeet.exception.ServerException
@@ -62,6 +64,11 @@ class GroupUserAddAdapter(userList: ArrayList<UserToGroupResponse>, activity: Gr
                 .placeholder(R.drawable.avatar_1)
                 .into(holder.profilePicture)
         }
+        holder.infoArea.setOnClickListener{
+            val newActivity = Intent(holder.itemView.context, UserShowActivity::class.java)
+                .putExtra("userId",model.id)
+            activity.startActivity(newActivity)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -74,6 +81,7 @@ class GroupUserAddAdapter(userList: ArrayList<UserToGroupResponse>, activity: Gr
         lateinit var email: TextView
         lateinit var sendButton: ImageButton
         lateinit var profilePicture: ImageView
+        lateinit var infoArea: LinearLayout
         var internalPosition=-1
 
         init {
@@ -82,6 +90,7 @@ class GroupUserAddAdapter(userList: ArrayList<UserToGroupResponse>, activity: Gr
             email = itemView.findViewById(R.id.group_user_add_email)
             sendButton = itemView.findViewById(R.id.group_user_add_BT)
             profilePicture = itemView.findViewById(R.id.group_add_user_avatar_IV)
+            infoArea = itemView.findViewById(R.id.group_add_user_info_area)
         }
     }
 
